cdb5afd25fb2488616a43e06bb6afad41b3badce
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-04-08 19:58:38 -0500

    basic constructor

--- src/com/jlinalg/matrices/Matrix.java
+++ src/com/jlinalg/matrices/Matrix.java
@@ -1,5 +1,29 @@
 package com.jlinalg.matrices;
 
-public class Matrix {
+import java.util.ArrayList;
 
+public class Matrix {
+	
+	private ArrayList<ArrayList<Double>> rows;
+	
+	
+	public Matrix(int numRows, int numCols, double filler) {
+		
+		this.rows = new ArrayList<ArrayList<Double>>();
+		
+		for (int r = 0; r < numRows; r++) {
+			ArrayList<Double> row = new ArrayList<Double>();
+			for (int c = 0; c < numCols; c++) {
+				row.add(filler);
+			}
+			this.rows.add(row);
+		}
+		
+	}
+	
+	
+	public Matrix(int numRows, int numCols) {
+		this(numRows, numCols, 0);
+	}
+	
 }
