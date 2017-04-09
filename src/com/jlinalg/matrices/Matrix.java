1e87325ff55b244762237774bd6759164023dd55
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-04-08 21:01:19 -0500

    setting the main diagonal and individual entries

--- src/com/jlinalg/matrices/Matrix.java
+++ src/com/jlinalg/matrices/Matrix.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 /**
  * Generates and performs basic operations on matrices of doubles.
  * @author Joe Cowman
+ * @since 2017-04-08
  */
 public class Matrix {
 	
@@ -47,6 +48,64 @@ public class Matrix {
 	 */
 	public Matrix(int numRows, int numCols) {
 		this(numRows, numCols, 0);
+	}
+	
+	/**
+	 * Returns the number of rows in the matrix.
+	 * @return the number of rows in the matrix
+	 */
+	public int numRows() {
+		return this.rows.size();
+	}
+	
+	/**
+	 * Returns the number of columns in the matrix.
+	 * @return the number of columns in the matrix
+	 */
+	public int numCols() {
+		return this.rows.get(0).size();
+	}
+	
+	/**
+	 * Sets the value of a single entry in the matrix.
+	 * @param row row at which the entry is to changed
+	 * @param col column at which the entry is to changed
+	 * @param value number to which the entry is changed
+	 * @return the value of the updated entry
+	 * @throws IllegalArgumentException if <code>row</code> or <code>col</code> are outside
+	 * the dimensions of the matrix.
+	 */
+	public double setEntry(int row, int col, double value) {
+		
+		if (row < 1 || row > this.numRows()) {
+			throw new IllegalArgumentException("Invalid row parameter.");
+		}
+		
+		if (col < 1 || col > this.numCols()) {
+			throw new IllegalArgumentException("Invalid column parameter.");
+		}
+		
+		this.rows.get(row-1).set(col-1, value);
+		return value;
+	}
+	
+	/**
+	 * Sets the value of every element in the main diagonal of the matrix.
+	 * @param value number to which each element in the main diagonal is changed
+	 * @return the new value of each element in the diagonal
+	 */
+	public double setDiagonal(double value) {
+		int rows = this.numRows();
+		int cols = this.numCols();
+		
+		for (int r = 0; r < rows; r++) {
+			if (r >= cols) {
+				return value;
+			}
+			this.rows.get(r).set(r, value);
+		}
+		
+		return value;
 	}
 	
 }
