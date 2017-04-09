b7d4b83bf44e58e16c67e98b523cb2ee47404352
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-04-08 20:27:40 -0500

    testing javadoc

--- src/com/jlinalg/matrices/Matrix.java
+++ src/com/jlinalg/matrices/Matrix.java
@@ -2,12 +2,30 @@ package com.jlinalg.matrices;
 
 import java.util.ArrayList;
 
+/**
+ * Generates and performs basic operations on matrices of doubles.
+ * @author Joe Cowman
+ */
 public class Matrix {
 	
 	private ArrayList<ArrayList<Double>> rows;
 	
-	
+	/**
+	 * Constructs a matrix with the given dimensions, with each element having the given value.
+	 * @param numRows the number of rows in the matrix
+	 * @param numCols the number of columns in the matrix
+	 * @param filler the value to fill every element in the matrix
+	 * @throws IllegalArgumentException if <code>numRows</code> or <code>numCols</code> are less than one
+	 */
 	public Matrix(int numRows, int numCols, double filler) {
+		
+		if (numRows <= 0) {
+			throw new IllegalArgumentException("Matrix must have at least one row.");
+		}
+		
+		if (numCols <= 0) {
+			throw new IllegalArgumentException("Matrix must have at least one column.");
+		}
 		
 		this.rows = new ArrayList<ArrayList<Double>>();
 		
@@ -21,7 +39,12 @@ public class Matrix {
 		
 	}
 	
-	
+	/**
+	 * Constructs a zero matrix with the given dimensions.
+	 * @param numRows the number of rows in the matrix
+	 * @param numCols the number of columns in the matrix
+	 * @throws IllegalArgumentException if <code>numRows</code> or <code>numCols</code> are less than one
+	 */
 	public Matrix(int numRows, int numCols) {
 		this(numRows, numCols, 0);
 	}
